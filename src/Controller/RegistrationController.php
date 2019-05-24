256c516431428ef08aef09f8b5550e7506ec0f48
Arman <armancho1@gmail.com>
2019-05-25 03:03:32 +0800

    Translations

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,13 +5,30 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
+/**
+ * Class RegistrationController
+ * @package App\Controller
+ */
 class RegistrationController extends AbstractController
 {
+    /**
+     * @Route("/", name="home")
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function home(Request $request)
+    {
+        return $this->redirectToRoute('app_register');
+    }
+
     /**
      * @Route("/register", name="app_register")
      *
@@ -33,19 +50,8 @@ class RegistrationController extends AbstractController
             ]);
         }
 
-        if($form->isValid())
-        {
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('password')->getData()
-                )
-            );
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
+        if($form->isValid()) {
+            $this->userAdd($user, $form, $passwordEncoder);
             $form = $this->createForm(RegistrationFormType::class, new User());
             $success = true;
         }
@@ -54,5 +60,20 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
             'success' => $success
         ]);
+    }
+
+    /**
+     * @param User $user
+     * @param FormInterface $form
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    protected function userAdd(User $user, FormInterface $form, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $pass = $passwordEncoder->encodePassword($user, $form->get('password')->getData());
+        $user->setPassword($pass);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
     }
 }
